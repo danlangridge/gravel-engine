8439ea2768e27a62873bbe4a673fc29dc714a97b
dan <dlangridge@gmail.com>
2014-01-21 23:04:42 -0800

    Added Spring class

---
+++ plinks.h
@@ -0,0 +1,29 @@
+#include <Particle.h>
+#include <PContact.h>
+
+
+class ParticleLink
+{
+public:
+  Particle* particle[2];
+  
+protected:
+
+  int currentLength() const;
+
+public:
+  virtual unsigned fillContact(ParticleContact *contact, unsigned limit) const = 0;
+
+};
+
+class ParticleCable : public ParticleLink
+{
+public:
+
+  int maxLength;
+
+  int restitution;
+
+  virtual unsigned fillContact(ParticleContact *contact, unsigned limit) const = 0;
+
+};
