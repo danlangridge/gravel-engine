4c744c1547a0fce517ffcb6a17eab63d976d186e
dan <dlangridge@gmail.com>
2014-01-23 17:07:18 -0800

    Implemented fillContact

--- plinks.cpp
+++ plinks.cpp
@@ -2,15 +2,28 @@
 #include <Vector.h>
 
 
-int ParticleLink::currentLink() const
-{
+int ParticleLink::currentLink() const {
   Vector relativePosition = particle[0]->getPosition() - particle[1]->getPosition();
 
   return relativePosition.magnitude();
 }
 
-unsigned ParticleCable::fillContact(ParticleContact *contact, unsigned limit) const
-{
+unsigned ParticleCable::fillContact(ParticleContact *contact, unsigned limit) const {
+  
   int length = currentLength();
-  return 0;
+
+  if (length < maxLength) return 0;
+  
+  contact->particle[0] = particle[0];
+  contact->particle[1] = particle[1];
+
+  Vector normal = particle[1]->getPosition() - particle[0]->getPosition();
+  normal.normalize();
+
+  contact->contactNormal = normal;
+
+  contact->penetration = length - maxLength;
+  contact->restitution = restitution;
+
+  return 1;
 }
