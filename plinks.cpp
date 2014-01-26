7ded40d0fa71aaa270d8345edc045f695a1e30d5
dan <dlangridge@gmail.com>
2014-01-25 19:15:04 -0800

    Added fillContact update to ParticleRod

--- plinks.cpp
+++ plinks.cpp
@@ -32,6 +32,17 @@ unsigned ParticleRod::fillContact( ParticleContact *contact, unsigned limit) con
   int currentLen = currentLength();
 
   if (currentLen == length) return 0;
+  
+  contact->particle[0] = particle[0];
+  contact->particle[1] = particle[1];
+
+  Vector normal = particle[1]->getPosition() - particle[0]->getPosition();
+
+  normal.normalize();
+  contact->contactNormal = normal;
+
+  contact->penetration = length-maxLength;
+  contact->restitution = restitution;
 
   return 1;
 }
