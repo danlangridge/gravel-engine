807f28b4f40d4140c6599dca0aadb41c5ba79dec
dan <dlangridge@gmail.com>
2014-01-30 22:19:44 -0800

    Added implementation of GenerateContacts

--- pworld.cpp
+++ pworld.cpp
@@ -9,3 +9,26 @@ void ParticleWorld::startFrame() {
     reg = reg->next;
   }
 }
+
+usigned ParticleWorld::generateContacts() {
+
+  unsigned limit = maxContacts;
+  ParticleContact *nextContact = contacts;
+
+  ContactGenRegistration *reg = firstContactGen;
+  
+  while (reg) {
+
+    unsigned used = reg->gen->addContact(nextContact, limit);
+    limit -= used;
+    nextContact += used;
+
+    if (limit  <= 0) break;
+
+    reg = reg->next;
+
+  }
+
+  return maxContacts - limit;
+
+}
