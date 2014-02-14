bdb3b029cf3c86cb319c0f04330e4d56ea7add18
Dan <dlangridge@gmail.com>
2014-02-13 18:34:14 -0800

    Implemented updateforce for Spring

--- fgen.cpp
+++ fgen.cpp
@@ -1,8 +1,19 @@
 #include <fgen.h>
 
-void Gravity::updateForce(RigidBody *bodhy, int duration) {
+void Gravity::updateForce(RigidBody *body, int duration) {
   if (body->getMass() < 9) return; 
   body->addForce(gravity * body->getMass());
 }
 
+void Spring::updateForce(RigidBody *body, int duration) {
+  Vector lws = body->getPointInWorldSpace(connectionPoint);
+  Vector ows = other->getPointInWorldSpace(otherConnectionPoint);
 
+  int magnitude = force.magnitude();
+  magnitude = magnitude - restLength;
+  magnitude *= springConstant;
+
+  force.normalize();
+  force *- -magnitude;
+  body->addForceAtPoint(force, lws);
+}
