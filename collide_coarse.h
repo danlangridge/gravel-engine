9fd1e8215f705ed7db25d0506d8e47cc3ee5e18b
Dan <dlangridge@gmail.com>
2014-02-17 23:14:18 -0800

    Added additional member functions to BVHNode

--- collide_coarse.h
+++ collide_coarse.h
@@ -10,5 +10,16 @@ class BVHNode {
   BVHNode* children[2];
   BoundingVolumeClass volume;
   RigidBody *body;
-  bool isLeaf() const;
-}; 
+  bool isLeaf() const
+  {
+    return (body != NULL);
+  }
+
+  unsigned getPotentialContacts(PotentialContact* contacts, unsigned limit) const;
+};
+
+template<class BoundingVolumeClass>
+bool BVHNode<BoundingVolumeClass>::overlaps( const BVHNode<BoundingVolumeClass>* other ) const
+{
+  return volume->overlaps(other->volume);
+} 
