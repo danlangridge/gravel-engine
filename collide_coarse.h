b72b05da7f95ef09c842f2a68aab6f092b3bf41a
Dan <dlangridge@gmail.com>
2014-02-21 10:17:55 -0800

    Added conditional when traversing PotentialContacts

--- collide_coarse.h
+++ collide_coarse.h
@@ -30,20 +30,29 @@ unsigned BVHNode <BoundingVolumeClass>::getPotentialContactsWith(
                                                                  PotentialContact* contacts,
                                                                  unsigned limit
                                                                 ) const
-{
-  if (!overlaps(other) || limit == 0) return 0;
-  if (isLeaf() && other->isLeaf()) {
-    contacts->body[0] = body;
-    contacts->body[1] = other->body;
-    return 1;
-  }
+  {
+    if (!overlaps(other) || limit == 0) return 0;
+    if (isLeaf() && other->isLeaf()) {
+      contacts->body[0] = body;
+      contacts->body[1] = other->body;
+      return 1;
+    }
 
-  if (other->isLeaf() || !isLeaf() && volume->getSize() >= other->volume->getSize()) {
-    unsigned count = children[0]->getPotentialContactsWith( other, contacts, limit);
+    if (other->isLeaf() || !isLeaf() && volume->getSize() >= other->volume->getSize()) {
+     unsigned count = children[0]->getPotentialContactsWith( other, contacts, limit);
     
     if (limit > count) {
-       return count + children[1]->getPotentialContactsWith(other, contacts+count,limit-count);
+     return count + children[1]->getPotentialContactsWith(other, contacts+count,limit-count);
     }
     else return count;
-  }   
+  }
+  else {
+    unsigned count = getPotentialContactsWith(other->children[0], contacts, limit);
+    if (limit > count) {
+      return count + getPotentialContactsWith( other->children[1], contacts+count, limit-count);
+    else {
+      return count;
+    } 
+   }  
+  }  
 } 
